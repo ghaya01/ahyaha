4f35336cbdfb6a6a654089dc1cb4a4b0b806bb08
AYA AYA <ayaa03442@gmail.com>
2025-03-17 16:12:04 +0100

    tp2

--- app/src/main/java/com/example/ahyaha/MainActivity.kt
+++ app/src/main/java/com/example/ahyaha/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.ahyaha.ui.FirstUI
-import com.example.ahyaha.theme.AhyahaTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ahyaha.ui.theme.AhyahaTheme
+import com.example.ahyaha.view.MainScreen
+import com.example.ahyaha.viewmodel.BloodTypeViewModel
+import com.example.ahyaha.viewmodel.DonorViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +20,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AhyahaTheme {
-                Scaffold { innerPadding ->
-                    FirstUI(modifier = Modifier.padding(innerPadding))
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    val donorViewModel = viewModel<DonorViewModel>()
+                    val bloodTypeViewModel = viewModel<BloodTypeViewModel>()
+
+
+                    MainScreen(
+                        donorViewModel,
+                        bloodTypeViewModel,
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
-}
+} 
